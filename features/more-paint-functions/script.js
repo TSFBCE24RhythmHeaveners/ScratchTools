9edb73f2750f683e637278eef054f2e56ab3891c
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-03-28 18:11:48 +0400

    Update script.js

--- features/more-paint-functions/script.js
+++ features/more-paint-functions/script.js
@@ -11,10 +11,6 @@ export default async function ({ feature, console, scratchClass }) {
       }
     }
 
-    for (var i in items) {
-      items[i].remove();
-    }
-
     paper.tool.onUpdateImage();
   }
 
